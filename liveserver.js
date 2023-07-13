480ccdf8a865added5e5e5e53e92ead651e212b8
GOKUL S <75518054+gokulstsk@users.noreply.github.com>
2023-07-13 18:39:58 +0530

    13-07-23eve EDIT

--- liveserver.js
+++ liveserver.js
@@ -11,6 +11,7 @@ var server=app.listen(8000,function(){
 });
 // var io=require('socket.io');
 var io=socket(server);
+var akms={};
 io.use('transports', ['websocket','polling']);
 console.log("server listening duplicate");
 
@@ -23,16 +24,37 @@ var io = require('socket.io')(server, {
         
     },
     allowEIO3: true 
-
 });
-
-
 
 io.on("connection",function(socket) {
     console.log("connection trying :: "+socket.id);
-    socket.emit("GetyourID",{id:socket.id});
+    
+    io.emit("GetyourID",{id:socket.id});
     socket.on("thank you ",function(){
         console.log("the server with id "+socket.id);
+    socket.on("IWasCreated",function(data){
+        if(data.id!=socket.id){
+            // cheat player code
+        }
+        akms[data.id]=data;
+        console.log(data);
+        socket.broadcast.emit("AnotherTankCreated",data);
+        for(key in akms){
+            if (key == socket.id) continue;
+            socket.emit("anotherTankcreated",akms[key]);
+        }
+    });
+    socket.on("IMoved", function (data) {
+        akms[data.id] = data;
+        socket.broadcast.emit("AnotherTankMoved", data);
+        
+    });
+
+    socket.on("IGoAway", function (data) {
+        delete akms[socket.id];
+        socket.broadcast.emit("AnotherWentAway", { id : socket.id });
+    });
+
     });
     // io.on("thankyou",function(){
     //     console.log("the server with id"+socket.id);
