ba636978f41508e24f443f4a5586a87450a4dee6
gokulstsk <gokulstsk@gmail.com>
2023-07-13 10:19:18 +0530

    new13072023

--- liveserver.js
+++ liveserver.js
@@ -1,23 +1,49 @@
+// import { Server } from "socket.io";
 var express = require('express');
 var socket = require('socket.io');
 var app = express();
 app.use("/", express.static(__dirname,));
 app.get('/', function(req, res) {
-    res.sendFile(__dirname +"/page1.html");
+    res.sendFile(__dirname+"/page2.html");
 });
-var server=app.listen(3000);
+var server=app.listen(8000,function(){
+    console.log("Server just started listening on port 8000....");
+});
+// var io=require('socket.io');
 var io=socket(server);
-var io = require("socket.io-client");
-
-io.connect('transport', ['websocket']);
+io.use('transports', ['websocket','polling']);
 console.log("server listening duplicate");
-socket=io.connect("http://localhost:3000",{transports:['websocket']});
-socket.on("connect",function(socket) {
-    // console.log("socket");
-    // console.log("connection trying"+socket.id);
-    // socket.emit("GetyourID",{id:socket.id});
-    // Socket.on("thankyou",function(){
-        // console.log("the server with id"+socket.id);
+
+var io = require('socket.io')(server, {
+    cors: {
+        origin: "http://localhost:8000",
+        methods: ["GET", "POST"],
+        transports: ['websocket', 'polling'],
+        credentials: true
+        
+    },
+    allowEIO3: true 
+
+});
+
+
+
+io.on("connection",function(socket) {
+    console.log("connection trying :: "+socket.id);
+    socket.emit("GetyourID",{id:socket.id});
+    socket.on("thank you ",function(){
+        console.log("the server with id "+socket.id);
     });
+    // io.on("thankyou",function(){
+    //     console.log("the server with id"+socket.id);
+    // });
+});
+
+// console.log('$socket.id :: '+socket.id);
+
+
+    // let http = require('http').Server(app);
+    // let port = process.env.PORT || 3000;
+
+    // console.log(`Server is up on port ${port}: http://localhost:${port}/`);
 // });
-// io.
